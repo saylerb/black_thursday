681434c7e40151dc80a8114f4a135eda03a21a09
saylerb <sayler.b@gmail.com>
2016-04-23 18:29:10 -0600

    add attributes for invoice item class

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,7 +1,24 @@
+require "bigdecimal"
+require "time"
+
 class InvoiceItem
 
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
   def initialize(row)
-    @row = row
+    @id = row[:id].to_i
+    @item_id = row[:item_id].to_i
+    @invoice_id = row[:invoice_id].to_i
+    @quantity = row[:quantity].to_i
+    @unit_price = BigDecimal.new(row[:unit_price])/100
+    @created_at = Time.parse(row[:created_at])
+    @updated_at = Time.parse(row[:updated_at])
   end
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,6 +1,8 @@
 require "minitest/autorun"
 require "minitest/emoji"
 require "./lib/invoice_item"
+require "time"
+require "bigdecimal"
 
 class InvoiceItemTest < Minitest::Test
 
@@ -16,6 +18,16 @@ class InvoiceItemTest < Minitest::Test
 
   def test_invoice_items_repo_exists
     assert @invoice
+  end
+
+  def test_invoice_has_attributes
+    assert_equal 1, @invoice.id
+    assert_equal 263519844, @invoice.item_id
+    assert_equal 1, @invoice.invoice_id
+    assert_equal 5, @invoice.quantity
+    assert_equal BigDecimal.new(13635)/100, @invoice.unit_price
+    assert_equal Time.parse("2012-03-27 14:54:09 UTC"), @invoice.created_at
+    assert_equal Time.parse("2012-03-27 14:54:09 UTC"), @invoice.updated_at
   end
 
 end
