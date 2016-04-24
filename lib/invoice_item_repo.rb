93d933e922725e4bee27e5d98ae9ecefde5f9232
saylerb <sayler.b@gmail.com>
2016-04-24 10:03:47 -0600

    add test find all by item id to InvoiceItemRepo

--- lib/invoice_item_repo.rb
+++ lib/invoice_item_repo.rb
@@ -4,7 +4,7 @@ require_relative "invoice_item"
 class InvoiceItemRepo
 
   def initialize(file_name = "./data/invoice_items.csv", sale_engine = nil)
-    @all_invoices = load_csv(file_name)
+    @all_invoice_items = load_csv(file_name)
   end
 
   def load_csv(file_name)
@@ -13,5 +13,27 @@ class InvoiceItemRepo
       InvoiceItem.new(row, self)
     end
 
+  end
+
+  def all
+    @all_invoice_items
+  end
+
+  def find_by_id(id)
+    @all_invoice_items.find do |invoice_item|
+      invoice_item.id == id
+    end
+  end
+
+  def find_all_by_item_id(item_id)
+    @all_invoice_items.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    @all_invoice_items.find_all do |invoice_item|
+      invoice_item.invoice_id == invoice_id
+    end
   end
 end
--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -5,15 +5,31 @@ require "./lib/invoice_item_repo"
 class InvoiceItemRepoTest < Minitest::Test
 
   def setup
-    @repo = InvoiceItemRepo.new("./fixtures/invoice_items_fixture.csv")
+    @repo = InvoiceItemRepo.new("./fixtures/invoice_items_sample.csv")
   end
 
   def test_invoice_items_repo_exists
     assert @repo
   end
 
-  def test_if_repo_contains_invoice_items
+  def test_if_repo_returns_invoice_items
+    assert_equal 100, @repo.all.length
+  end
 
+  def test_that_repo_can_find_by_invoice_id
+    assert_kind_of InvoiceItem, @repo.find_by_id(10)
+    assert_equal 263523644, @repo.find_by_id(10).item_id
+  end
+
+  def test_that_repo_can_find_all_by_item_id
+    result_1 = @repo.find_all_by_item_id(263529264)
+    result_2 =   @repo.find_all_by_item_id(263553176)
+    assert_kind_of Array, result_1
+    assert_kind_of Array, result_2
+    assert_equal 2, result_1.length
+    assert_equal 2, result_2.length
+    assert_kind_of InvoiceItem, result_1[0]
+    assert_kind_of InvoiceItem, result_2[0]
   end
 
 end
