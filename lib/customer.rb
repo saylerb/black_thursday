d4416760a5584498f632184f0d97db134f33aa3d
saylerb <sayler.b@gmail.com>
2016-04-24 10:26:30 -0600

    complete customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,26 +1,22 @@
 require "bigdecimal"
 require "time"
 
-class InvoiceItem
+class Customer
 
   attr_reader :id,
-              :item_id,
-              :invoice_id,
-              :quantity,
-              :unit_price,
+              :first_name,
+              :last_name,
               :created_at,
               :updated_at
 
-  def initialize(row, invoice_item_repository)
+  def initialize(row, customer_repository)
     @id = row[:id].to_i
-    @item_id = row[:item_id].to_i
-    @invoice_id = row[:invoice_id].to_i
-    @quantity = row[:quantity].to_i
-    @unit_price = BigDecimal.new(row[:unit_price])/100
+    @first_name = row[:first_name]
+    @last_name = row[:last_name]
     @created_at = Time.parse(row[:created_at])
     @updated_at = Time.parse(row[:updated_at])
 
-    @invoice_item_repo = invoice_item_repository
+    @customer_repo = customer_repository
   end
 
 end
