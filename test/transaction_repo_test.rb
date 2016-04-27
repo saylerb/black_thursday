eef35258addec237d2e42381d443945a64ed4f97
saylerb <sayler.b@gmail.com>
2016-04-26 23:27:01 -0600

    add another test for transaction repo

--- test/transaction_repo_test.rb
+++ test/transaction_repo_test.rb
@@ -15,6 +15,12 @@ class TransactionRepoTest < Minitest::Test
     assert @repo
   end
 
+  def test_if_it_can_load_all_transactions
+    all_transactions = @repo.all
+    assert_kind_of Array, all_transactions 
+    assert_equal 100, all_transactions.length #4985 in live data
+  end
+
   def test_if_transaction_array_created
     assert_equal Array, @repo.all_transactions.class
   end
@@ -37,9 +43,15 @@ class TransactionRepoTest < Minitest::Test
     assert_equal 4068631943231473, @repo.find_by_id(1).credit_card_number
   end
 
+  def test_it_finds_all_by_credit_card_number
+    trans = @repo.find_all_by_credit_card_number(4068631943231473)
+    assert_kind_of Array, trans
+    assert_equal 1, trans.first.id
+  end
+
   def test_it_finds_all_transactions_by_result
     assert_equal 77, @repo.find_all_by_result("success").count
     assert_equal 23, @repo.find_all_by_result('failed').count
   end
 
-end
+ end
